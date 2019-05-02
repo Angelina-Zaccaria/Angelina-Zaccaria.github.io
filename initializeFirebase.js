aeae78bfbb84b75331d3e8c5c85e41e194d65df3
Angelina Zaccaria <angelina.zaccaria@gmail.com>
2019-05-02 01:48:24 -0400

    commit 4

---
+++ initializeFirebase.js
@@ -0,0 +1,21 @@
+//Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDTNpbKA9jgDdgdvacNWCkPCwSWGaU-m1E",
+    authDomain: "ssw-215-project.firebaseapp.com",
+    databaseURL: "https://ssw-215-project.firebaseio.com",
+    projectId: "ssw-215-project",
+    storageBucket: "ssw-215-project.appspot.com",
+    messagingSenderId: "705378774344"
+};
+firebase.initializeApp(config);
+
+//Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB4qPNgewB3Rl3WGZlo1VwiCqI6dKaO2mY",
+    authDomain: "stevens-ssw.firebaseapp.com",
+    databaseURL: "https://stevens-ssw.firebaseio.com",
+    projectId: "stevens-ssw",
+    storageBucket: "stevens-ssw.appspot.com",
+    messagingSenderId: "340247076403"
+};
+firebase.initializeApp(config);
